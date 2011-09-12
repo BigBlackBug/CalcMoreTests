10e24ede33857f9fcc97b203b4fd199661126522
iliax <iliax@iliax-desktop>
2011-09-12 17:02:25 +0400

    init2

---
+++ app/controllers/Application.java
@@ -0,0 +1,42 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+
+import java.util.*;
+import models.WordCountPair;
+import play.data.validation.Required;
+
+
+public class Application extends Controller {
+
+    
+    public static void index() {
+        
+        List<WordCountPair> countPairs=WordCountPair.all().fetch();
+        render(countPairs);
+    }
+
+    
+    public static void saveWord(@Required String word, @Required String count){
+        try {
+            Long longCount=Long.parseLong(count.trim());
+            WordCountPair  existed;
+            
+            if((existed = WordCountPair.all().filter("word", word).get()) ==null){
+                new WordCountPair(word.trim(), longCount).save();
+                System.out.println(" saved!");
+            } else {
+                existed.count+=longCount;
+                existed.update();
+                System.out.println(" up!");
+            }
+            
+        }
+        catch(NumberFormatException  nfe){
+            System.err.print("cant parse string to long");
+        }
+        
+        index();
+    }
+}
---
+++ app/models/WordCountPair.java
@@ -0,0 +1,60 @@
+package models;
+
+import java.math.BigDecimal;
+import siena.*;
+
+
+/**
+ *
+ * @author iliax
+ */
+public class WordCountPair extends Model {
+    
+    @Id
+    public Long id;
+    
+    public String word;
+    
+    public Long  count;
+
+    public WordCountPair(String word, Long count) {
+        this.word = word;
+        this.count = count;
+    }
+
+    public WordCountPair() {
+    }
+    
+    
+    public static Query<WordCountPair> all(){
+        return WordCountPair.all(WordCountPair.class);
+    }
+
+    @Override
+    public String toString() {
+        return word+" - "+count;
+    }
+
+    @Override
+    public boolean equals(Object that) {
+        if(that==null)
+            return false;
+        
+        if(that instanceof WordCountPair){
+            WordCountPair pair=(WordCountPair)that;
+            return ((pair.word.equals(word))&&(pair.count.equals(count)));
+        }
+        
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 79 * hash + (this.word != null ? this.word.hashCode() : 0);
+        hash = 79 * hash + (this.count != null ? this.count.hashCode() : 0);
+        return hash;
+    }
+    
+    
+}
